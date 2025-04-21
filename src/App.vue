c7e61ea0ec07dd87fa559464fdd0cc5888eff39d
Jakub Hajduk <jakubhajduk53@gmail.com>
2025-04-21 20:42:32 +0200

    feat: add buttonview

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
 <script setup lang="ts">
-import Button from "./components/Button.vue";
-import Input from "./components/Input.vue";
+import ButtonView from "./views/ButtonView.vue";
 </script>
 
 <template>
-  <div class="flex gap-5">
-    <Button color="primary" text="primary" />
-    <Button color="secondary" text="secondary" />
-    <Button color="tertiary" text="tertiary" />
-    <Button color="warning" text="warning" />
-    <Button color="danger" text="danger" />
-    <Button color="info" text="info" />
-    <Button color="success" text="success" />
-  </div>
-  <Input />
+  <ButtonView />
 </template>
 
 <style scoped></style>
---
+++ src/views/ButtonView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Button from "../components/Button.vue";
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center gap-5">
+    <p class="text-3xl md:text-5xl font-bold">Buttons</p>
+    <div class="flex flex-col items-center justify-center gap-5">
+      <p class="text-xl md:text-3xl font-semibold">Basic themes</p>
+      <div class="flex gap-5">
+        <Button color="primary" text="primary" />
+        <Button color="secondary" text="secondary" />
+        <Button color="tertiary" text="tertiary" />
+      </div>
+      <p class="text-xl md:text-3xl font-semibold mt-5">Situational themes</p>
+      <div class="flex gap-5">
+        <Button color="success" text="success" />
+        <Button color="warning" text="warning" />
+        <Button color="danger" text="danger" />
+        <Button color="info" text="info" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
