ff4eb4b86b214d9181319a2fadf981b5e52f5f17
Jakub Hajduk <jakubhajduk53@gmail.com>
2025-04-23 12:18:31 +0200

    feature: update mainpage

--- src/views/MainPageView.vue
+++ src/views/MainPageView.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from "../components/Button.vue";
+</script>
 
-<template>MainPage</template>
+<template>
+  <div
+    class="flex flex-col gap-10 items-center justify-center text-center -translate-y-25 h-full w-full"
+  >
+    <p class="text-3xl md:text-5xl font-bold">Welcome to MyUI</p>
+    <p class="text-xl md:text-3xl w-[30em]">
+      Build beautiful, responsive interfaces with ease.<br />
+      Designed for speed, clarity, and creativity
+    </p>
+    <RouterLink to="/button">
+      <Button color="primary" size="large" text="Get Started" />
+    </RouterLink>
+  </div>
+</template>
 
 <style scoped></style>
