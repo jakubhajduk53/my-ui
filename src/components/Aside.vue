640fb12f8d6053bf18a221197d2f2a24ae1ff9de
Jakub Hajduk <jakubhajduk53@gmail.com>
2025-05-03 10:38:13 +0200

    feat: update aside

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const isOpen = ref<boolean>(false);
+const isOpen = ref<boolean>(true);
 
 const switchAside = () => {
   isOpen.value = !isOpen.value;
@@ -23,27 +23,32 @@ const switchAside = () => {
     >
       <RouterLink
         to="/"
-        class="py-3 w-full text-xl md:text-3xl font-bold cursor-pointer bg-[hsl(26,18%,45%)] border-b"
+        class="py-3 w-full text-xl md:text-3xl font-bold cursor-pointer bg-radial from-[hsl(26,18%,50%)] to-[hsl(26,18%,45%)] hover:text-[hsl(26,68%,10%)] transition duration-100"
         >Main Page
       </RouterLink>
       <div class="w-full bg-[hsl(26,68%,92%)]" />
       <RouterLink
         to="/button"
-        class="py-3 w-full text-xl md:text-3xl font-semibold cursor-pointer bg-[hsl(26,18%,50%)] border-b"
+        class="py-3 w-full text-xl md:text-3xl font-semibold cursor-pointer bg-radial from-[hsl(26,18%,55%)] to-[hsl(26,18%,50%)] hover:text-[hsl(26,68%,10%)] transition duration-100"
         >Button
       </RouterLink>
       <RouterLink
         to="/color"
-        class="py-3 w-full text-xl md:text-3xl font-semibold cursor-pointer bg-[hsl(26,18%,50%)] border-b"
+        class="py-3 w-full text-xl md:text-3xl font-semibold cursor-pointer bg-radial from-[hsl(26,18%,55%)] to-[hsl(26,18%,50%)] hover:text-[hsl(26,68%,10%)] transition duration-100"
         >Color
       </RouterLink>
     </div>
 
     <div
       @click="switchAside"
-      class="flex items-center px-1 md:px-3 text-xl md:text-3xl cursor-pointer bg-gradient-to-l from-[hsl(26,18%,55%)] to-[hsl(26,18%,45%)] border-l"
+      class="flex items-center text-xl md:text-3xl cursor-pointer bg-gradient-to-l from-[hsl(26,18%,55%)] to-[hsl(26,18%,45%)]"
     >
       <i :class="isOpen ? 'bx bx-caret-left' : 'bx bx-caret-right'" />
     </div>
   </div>
 </template>
+<style scoped>
+.router-link-active {
+  color: hsl(26, 68%, 10%);
+}
+</style>
