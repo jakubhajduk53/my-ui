a226a0be9ae5df45719b8d654408dc4155870cd8
Jakub Hajduk <jakubhajduk53@gmail.com>
2025-05-21 11:18:46 +0200

    feat: update aside styles

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -11,42 +11,41 @@ const switchAside = () => {
 <template>
   <div
     :class="[
-      'fixed top-0 left-0 z-10 flex h-screen transition-transform duration-300 ease-in-out text-font-color ',
+      'fixed top-0 left-0 z-10 flex h-screen transition-transform duration-300 ease-in-out bg-[hsl(26,68%,97%)]',
       isOpen
         ? 'translate-x-0'
         : '-translate-x-[50vw] sm:-translate-x-[30vw] md:-translate-x-[15vw]',
     ]"
   >
     <div
-      id="asideContainer"
       class="flex flex-col h-full items-center text-center w-[50vw] sm-[30vw] md:w-[15vw]"
     >
       <RouterLink
         to="/"
-        class="py-3 w-full text-xl md:text-3xl font-bold cursor-pointer bg-radial from-[hsl(26,18%,50%)] to-[hsl(26,18%,45%)] hover:text-[hsl(26,68%,10%)] transition duration-100"
+        class="py-3 w-full text-xl md:text-3xl font-bold cursor-pointer bg-radial transition duration-100 border-b border-[hsla(26,68%,20%,10%)] hover:text-[hsl(26,20%,15%)]"
         >Main Page
       </RouterLink>
       <div class="w-full bg-[hsl(26,68%,92%)]" />
       <RouterLink
         to="/button"
-        class="py-3 w-full text-xl md:text-3xl font-semibold cursor-pointer bg-radial from-[hsl(26,18%,55%)] to-[hsl(26,18%,50%)] hover:text-[hsl(26,68%,10%)] transition duration-100"
+        class="py-3 w-full text-xl md:text-3xl font-semibold cursor-pointer transition duration-100 border-b border-[hsla(26,68%,20%,10%)] hover:text-[hsl(26,20%,15%)]"
         >Button
       </RouterLink>
       <RouterLink
         to="/color"
-        class="py-3 w-full text-xl md:text-3xl font-semibold cursor-pointer bg-radial from-[hsl(26,18%,55%)] to-[hsl(26,18%,50%)] hover:text-[hsl(26,68%,10%)] transition duration-100"
+        class="py-3 w-full text-xl md:text-3xl font-semibold cursor-pointer transition duration-100 border-b border-[hsla(26,68%,20%,10%)] hover:text-[hsl(26,20%,15%)]"
         >Color
       </RouterLink>
       <RouterLink
         to="/code"
-        class="py-3 w-full text-xl md:text-3xl font-semibold cursor-pointer bg-radial from-[hsl(26,18%,55%)] to-[hsl(26,18%,50%)] hover:text-[hsl(26,68%,10%)] transition duration-100"
+        class="py-3 w-full text-xl md:text-3xl font-semibold cursor-pointer transition duration-100 border-b border-[hsla(26,68%,20%,10%)] hover:text-[hsl(26,20%,15%)]"
         >Code Display
       </RouterLink>
     </div>
 
     <div
       @click="switchAside"
-      class="flex items-center text-xl md:text-3xl cursor-pointer bg-gradient-to-l from-[hsl(26,18%,55%)] to-[hsl(26,18%,45%)]"
+      class="flex items-center text-xl md:text-3xl cursor-pointer bg-gradient-to-l from-[hsl(26,68%,90%)] shadow-[3px_0px_3px_hsla(26,68%,20%,15%)]"
     >
       <i :class="isOpen ? 'bx bx-caret-left' : 'bx bx-caret-right'" />
     </div>
@@ -54,6 +53,8 @@ const switchAside = () => {
 </template>
 <style scoped>
 .router-link-active {
-  color: hsl(26, 68%, 10%);
+  color: hsl(26, 20%, 15%);
+  text-decoration: underline;
+  text-underline-offset: 5px;
 }
 </style>
